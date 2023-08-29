13d6b3585cc59edd78e9890f725264ed5e0cb80e
Celio Caus Junior <celiocausjunior@gmail.com>
2023-08-29 15:44:58 -0300

    SaleRepository query for Report with parameters upper case to select name

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-    @Query(nativeQuery = true, value = "SELECT * FROM tb_sales INNER JOIN tb_sellers ON tb_sales.seller_id = tb_sellers.id WHERE tb_sales.date BETWEEN :minDate AND :maxDate AND (:name IS NULL OR tb_sellers.name = :name) ORDER BY tb_sales.date DESC")
+    @Query(nativeQuery = true, value = "SELECT * FROM tb_sales INNER JOIN tb_sellers ON tb_sales.seller_id = tb_sellers.id WHERE tb_sales.date BETWEEN :minDate AND :maxDate AND (:name IS NULL OR UPPER(tb_sellers.name) = UPPER(:name)) ORDER BY tb_sales.date DESC")
     List<Sale> search1(LocalDate minDate, LocalDate maxDate, String name);
 }
